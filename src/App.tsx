0a4c41757b5cebfc5c14d5e60ac017f55dc87ec6
Ionel Lupu <ionel.cristian.lupu@gmail.com>
2024-01-04 14:04:03 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button } from 'react-aria-components'
+
+function App() {
+
+    return (
+        <>
+            <Button>button</Button>
+        </>
+    )
+}
+
+export default App
